0b406df77b4cdc1cb385b0dd76caf80bb40db3fb
Radifan Mardhi Prana <43719706+radifanmp@users.noreply.github.com>
2020-09-29 10:32:30 +0700

    Consept

---
+++ src/Components/Content.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+
+// export default class Content extends Component {
+//   render() {
+//     return (
+//       <div className="App-header">
+//         <p>Selamat Data Batch #18 Botcamp Dumbways.id</p>
+//       </div>
+//     );
+//   }
+// }
+
+function Content() {
+  return (
+    <div className="App-header">
+      <p>Selamat Data Batch #18 Botcamp Dumbways.id</p>
+    </div>
+  );
+}
+
+export default Content;
---
+++ src/Components/Header.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+
+// class Header extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <h1>This is Header, ini header Loh, inget Header</h1>
+//       </div>
+//     );
+//   }
+// }
+
+function Header() {
+  return (
+    <div>
+      <h1>This is Header, ini header Loh, inget Header</h1>
+    </div>
+  );
+}
+
+export default Header;
